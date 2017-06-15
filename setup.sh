aeeebdffeff6757369978c2b8f9f5acd7beacee5
Michael Strasser <mst1409@gmx.at>
2017-06-15 13:47:01 +0200

    Added symlinking of .vimrc and .zshrc

--- setup.sh
+++ setup.sh
@@ -6,16 +6,25 @@ fi
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 if [ $? -ne 0 ]; then
 	echo "Cloning FZF failed, exiting"
-	exit 1
+	#exit 1
 fi
 ~/.fzf/install
 if [ $? -ne 0 ]; then
 	echo "Installing FZF failed, exiting"
-	exit 1
+	#exit 1
 fi
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 if [ $? -ne 0 ]; then
 	echo "Cloning Vundle failed, exiting"
-	exit 1
+	#exit 1
 fi
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+echo "Overwriting .vimrc and .zshrc..."
+mv $HOME/.vimrc $HOME/.vimrc.bak
+mv $HOME/.zshrc $HOME/.zshrc.bak
+echo "Backed up old files as .vimrc.bak and .zshrc.bak"
+ln -s $SCRIPTPATH/.vimrc $HOME/.vimrc
+ln -s $SCRIPTPATH/.zshrc $HOME/.zshrc
+echo "Set up symlinks to .dotfiles repo for .vimrc and .zshrc"
+echo "Installing vim plugins..."
 vim +PlugInstall +qall
